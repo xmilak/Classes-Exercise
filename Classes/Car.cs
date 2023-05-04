a04310a69396a4a87f1073c3df747124f660dc15
xmilak <126503532+xmilak@users.noreply.github.com>
2023-05-03 21:09:49 -0400

    Done

---
+++ Classes/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+            
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public void Data()
+        {
+            Console.WriteLine($"You are asking about your {Year} {Make} {Model}");
+           
+        }
+        
+    
+    
+        
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Classes
+﻿using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car old = new Car();
+
+            old.Make = "BMW";
+            old.Year = "1996";
+            old.Model = "E36";
+
+
+            old.Data();
+
         }
     }
 }
